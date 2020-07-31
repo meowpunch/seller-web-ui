b735e723e374a47b5a0da71e5f991eb7553d7c8d
meowpunch <jinhoonbae@gmail.com>
2020-08-01 04:39:13 +0900

    JB: add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {routes} from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const switchRoutes = (
+    <Switch>
+        {routes.map((r, k) => {
+            return (
+                <Route
+                    path={r.path}
+                    component={r.component}
+                    key={k}
+                />
+            )
+        })}
+        <Redirect from="/" to="/dashboard"/>
+    </Switch>
+)
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            {switchRoutes}
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index: React.FC = () => {
+    return (
+        <div>
+            dashboard
+        </div>
+    )
+}
+
+export default Index
